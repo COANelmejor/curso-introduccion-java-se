9530a2858c218085bb747ba093c316dd705cc5b5
Christian O. Arana Navas <COANelmejor@gmail.com>
2023-10-09 11:51:00 -0600

    Naming convention examples

---
+++ src/NamingJava.java
@@ -0,0 +1,27 @@
+public class NamingJava {
+    public static void main(String[] args) {
+
+        // Java is case-sensitive on variables
+        int celphone = 33337777;
+        int celPhone = 55553333;
+
+        System.out.println(celphone);
+        System.out.println(celPhone);
+
+        // Variable names can't start with numbers, but they can start with $ or _
+        String $countryName = "Spain";
+        String _backgroundColor = "Green";
+        String currency$ = "MXN";
+        String background_color = "BLUE";
+
+        // Constants are written in uppercase and separated by underscores
+        // This is a convention, not a rule
+        final int POSITION = -5;
+        int MAX_WIDTH = 9999;
+        int MIN_WIDTH = 1;
+
+        // Lower camel case
+        String fullName = "Luis Diaz de Leon";
+        int sizeInCentimeters = 26;
+    }
+}
