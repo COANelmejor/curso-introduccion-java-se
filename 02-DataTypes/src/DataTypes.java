72e8afe611b3cec8d385b59d8258f60832bcfad7
Christian O. Arana Navas <COANelmejor@gmail.com>
2023-10-09 12:23:45 -0600

    Add Numbre types examples

---
+++ 02-DataTypes/src/DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // int. Max value: 2,147,483,647
+        int n = 1234567890;
+
+        // short. Max value: 32,767
+        short s = 12345;
+
+        // long. Max value: 9,223,372,036,854,775,807
+        long l = 1234567890123456789L;
+
+        // float. Max value: 3.4028235E38
+        float f = 3.14159265358979323846264338327950288419716939937510582097494459230781640628620899f;
+
+        // double. Max value: 1.7976931348623157E308
+        double d = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089d;
+
+        // byte. Max value: 127
+        byte b = 127;
+    }
+}
